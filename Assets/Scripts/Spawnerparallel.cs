6dd71007cea5059a7b9440fc28bba2344a62db73
Senjucasie <paulkpraveen@gmail.com>
2025-04-06 20:26:55 +0530

    created  a job handler to move multiple objects

--- Assets/Scripts/Spawnerparallel.cs
+++ Assets/Scripts/Spawnerparallel.cs
@@ -1,5 +1,12 @@
+using System;
 using System.Collections.Generic;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Jobs;
+
 
 public class Spawnerparallel : MonoBehaviour
 {
@@ -7,6 +14,8 @@ public class Spawnerparallel : MonoBehaviour
     [SerializeField]private GameObject _sheepPF;
     private List<GameObject> _sheeps = new();
 
+    private JobHandle _moveHandler;
+    private TransformAccessArray _transformAccessArray;
 
     private void Start()
     {
@@ -25,16 +34,53 @@ public class Spawnerparallel : MonoBehaviour
             }
             startpos = new Vector3(-700, -125, startpos.z +5);
         }
+
+        SetTranformArray();
+
+    }
+    private void SetTranformArray()
+    {
+        Transform[] dummy = new Transform[_sheeps.Count];
+        
+        for(int i = 0;i<_sheeps.Count;i++)
+        {
+            dummy[i] =_sheeps[i].transform;
+        }
+        _transformAccessArray = new TransformAccessArray(dummy);
     }
     private void Update()
     {
-        foreach (GameObject sheep in _sheeps)
+        MoveJob movejob = new MoveJob(_speed,Time.deltaTime);
+        _moveHandler = movejob.Schedule(_transformAccessArray);
+
+    }
+    private void LateUpdate()
+    {
+       _moveHandler.Complete();
+    }
+
+    struct MoveJob : IJobParallelForTransform
+    {
+         private float _speed;
+        private float _deltaTime;
+        public MoveJob(float speed,float deltatime)
         {
-            sheep.transform.Translate(Vector3.forward * _speed * Time.deltaTime);
-            if (sheep.transform.position.z > 192)
+            _deltaTime = deltatime;
+            _speed = speed;
+        }
+       
+        public void Execute(int index, TransformAccess transform)
+        {
+            transform.position += (Vector3.forward* _speed*_deltaTime);
+            if(transform.position.z >192)
             {
-                sheep.transform.position = new Vector3(sheep.transform.position.x, sheep.transform.position.y, -409f);
+                transform.position = new Vector3(transform.position.x, transform.position.y, -409f);
             }
         }
+    }
+
+    private void OnDestroy()
+    {
+        _transformAccessArray.Dispose();
     }
 }
